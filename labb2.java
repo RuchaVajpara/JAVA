f82998c01cc02a7191c1bb55721ea6c911ab2b13
Vajpara Rucha <vajpararucha@gmail.com>
2025-02-02 18:43:24 +0530

    area of rectangle

---
+++ labb2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class labb2
+{
+	public static void main(String[]args)
+	{
+	System.out.println("area of rectangle");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("length of rectangle");
+	int a=sc.nextInt();
+	System.out.println("width of rectangle");
+	int b=sc.nextInt();
+	 System.out.println( "area of rectangle="+a*b);
+
+	}
+}
